50ab3e3dfe624b5e58a746d86c6b9fffe325e0ae
Rique <166674674+HenriqueIgreja@users.noreply.github.com>
2026-04-25 13:57:26 -0300

    Clean Nest Project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GamesModule } from './games/games.module';
-import { VotesModule } from './votes/votes.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [GamesModule, VotesModule, AuthModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
