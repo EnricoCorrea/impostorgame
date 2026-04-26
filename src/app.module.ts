7b0468c7e5295828ce41abb1df2b69243a489444
EnricoCorrea <enricocorrea0504@gmail.com>
2026-04-26 00:39:27 -0300

    fix module app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomsModule } from './rooms/rooms.module';
 import { UsersModule } from './users/users.module';
-import { GamesModule } from './games/games.module';
+import { GamesModule } from './games/games.module'; 
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [RoomsModule, UsersModule, GamesModule],
+  imports: [RoomsModule, UsersModule, GamesModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'amigo',
+      password: 'Cefet123!',
+      database: 'impostorgame',
+
+      autoLoadModels: true,
+      synchronize: false,
+
+      logging: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
