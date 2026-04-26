3b91221d322feb4080b975c6989fcda33bf4da0f
EnricoCorrea <enricocorrea0504@gmail.com>
2026-04-26 00:49:09 -0300

    sequelize do user.entitiy e imports

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Model, Table } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'users',
+  timestamps: false,
+})
+export class User extends Model {
+  @Column
+  name: string;
+
+  @Column
+  email: string;
+
+  @Column
+  password: string;
+}
