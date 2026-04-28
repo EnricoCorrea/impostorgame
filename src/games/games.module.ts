0a3c52e8ca485b939e3d2354ab10cfd5a0616dd0
EnricoCorrea <enricocorrea0504@gmail.com>
2026-04-28 09:18:04 -0300

    fixed

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { GamesService } from './games.service';
 import { GamesController } from './games.controller';
-import { SequelizeModule } from '@nestjs/sequelize';
+
 import { Game } from './entities/game.entity';
+import { Player } from 'src/players/entities/player.entity';
+import { Vote } from 'src/votes/entities/vote.entity';
+import { Room } from 'src/rooms/entities/room.entity';
 
 @Module({
+  imports: [
+    SequelizeModule.forFeature([
+      Game,
+      Player,
+      Vote,
+      Room,
+    ]),
+  ],
   controllers: [GamesController],
   providers: [GamesService],
-  imports: [SequelizeModule.forFeature([Game])],
 })
 export class GamesModule {}
