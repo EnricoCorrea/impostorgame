98833dcf639d0407e187f6820a0718f8c3074afb
meliij <melissajqt18@gmail.com>
2026-06-23 22:59:21 -0300

    atualizando dependencias

--- src/teste.ts
+++ src/teste.ts
@@ -1,20 +1,20 @@
-const { io } = require("socket.io-client");
+const { io } = require('socket.io-client');
 
-const socket = io("http://localhost:3001/", {
-  transports: ["websocket"],
+const socket = io('http://localhost:3001/', {
+  transports: ['websocket'],
 });
 
-socket.on("connect", () => {
-  console.log("connected:", socket.id);
+socket.on('connect', () => {
+  console.log('connected:', socket.id);
 
-  socket.emit("join_room", { roomId: 1 });
+  socket.emit('join_room', { roomId: 1 });
 
-  socket.emit("start_game", {
+  socket.emit('start_game', {
     gameId: 1,
     roomId: 1,
   });
 
-  socket.emit("vote", {
+  socket.emit('vote', {
     gameId: 1,
     userId: 1,
     targetId: 2,
@@ -22,10 +22,10 @@ socket.on("connect", () => {
   });
 });
 
-socket.on("game_updated", (data) => {
-  console.log("GAME UPDATE:", data);
+socket.on('game_updated', (data) => {
+  console.log('GAME UPDATE:', data);
 });
 
-socket.on("connect_error", (err) => {
-  console.log("connection error:", err.message);
+socket.on('connect_error', (err) => {
+  console.log('connection error:', err.message);
 });
