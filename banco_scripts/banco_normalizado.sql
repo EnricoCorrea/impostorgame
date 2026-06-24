81aa3405226b5ae3b43b7ec8f12abe255b81653b
meliij <melissajqt18@gmail.com>
2026-06-23 22:40:39 -0300

    Banco + seeds atualizados

--- banco_scripts/banco_normalizado.sql
+++ banco_scripts/banco_normalizado.sql
@@ -16,116 +16,128 @@ CREATE TYPE player_role AS ENUM (
   'INNOCENT'
 );
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) UNIQUE NOT NULL,
-  email VARCHAR(150) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+
+CREATE TABLE public.clues (
+	id serial4 NOT NULL,
+	game_id int4 NOT NULL,
+	player_id int4 NOT NULL,
+	round_number int4 NOT NULL,
+	clue text NOT NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT clues_pkey PRIMARY KEY (id),
+	CONSTRAINT clues_game_id_fkey FOREIGN KEY (game_id) REFERENCES public.games(id) ON DELETE CASCADE,
+	CONSTRAINT clues_player_id_fkey FOREIGN KEY (player_id) REFERENCES public.players(id) ON DELETE CASCADE
+);
+CREATE INDEX idx_clues_game ON public.clues USING btree (game_id);
+
+
+CREATE TABLE public.game_words (
+	game_id int4 NOT NULL,
+	word_id int4 NOT NULL,
+	CONSTRAINT game_words_pkey PRIMARY KEY (game_id),
+	CONSTRAINT game_words_game_id_fkey FOREIGN KEY (game_id) REFERENCES public.games(id) ON DELETE CASCADE,
+	CONSTRAINT game_words_word_id_fkey FOREIGN KEY (word_id) REFERENCES public.words(id)
 );
 
-CREATE TABLE rooms (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  host_id INT NOT NULL,
-  status room_status DEFAULT 'WAITING' NOT NULL,
-  max_users INT DEFAULT 3 NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  closed_at TIMESTAMP,
 
-  FOREIGN KEY (host_id) REFERENCES users(id) ON DELETE CASCADE
+CREATE TABLE public.games (
+	id serial4 NOT NULL,
+	room_id int4 NOT NULL,
+	winner public."player_role" NULL,
+	status public."game_status" DEFAULT 'WAITING'::game_status NOT NULL,
+	round_number int4 DEFAULT 1 NOT NULL,
+	started_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	finished_at timestamp NULL,
+	CONSTRAINT games_pkey PRIMARY KEY (id),
+	CONSTRAINT games_room_id_fkey FOREIGN KEY (room_id) REFERENCES public.rooms(id) ON DELETE CASCADE
 );
 
-CREATE TABLE room_players (
-  room_id INT,
-  player_id INT,
-  joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 
-  PRIMARY KEY (room_id, player_id),
 
-  FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE,
-  FOREIGN KEY (player_id) REFERENCES users(id) ON DELETE CASCADE
+CREATE TABLE public.messages (
+	id serial4 NOT NULL,
+	game_id int4 NOT NULL,
+	player_id int4 NOT NULL,
+	"content" text NOT NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT messages_pkey PRIMARY KEY (id),
+	CONSTRAINT messages_game_id_fkey FOREIGN KEY (game_id) REFERENCES public.games(id) ON DELETE CASCADE,
+	CONSTRAINT messages_player_id_fkey FOREIGN KEY (player_id) REFERENCES public.players(id) ON DELETE CASCADE
+);
+CREATE INDEX idx_messages_game ON public.messages USING btree (game_id);
+
+
+CREATE TABLE public.players (
+	id int4 DEFAULT nextval('players_player_id_seq'::regclass) NOT NULL,
+	game_id int4 NOT NULL,
+	user_id int4 NOT NULL,
+	word_id int4 NOT NULL,
+	"role" public."player_role" DEFAULT 'INNOCENT'::player_role NOT NULL,
+	is_alive bool DEFAULT true NOT NULL,
+	CONSTRAINT players_game_id_user_id_key UNIQUE (game_id, user_id),
+	CONSTRAINT players_pkey PRIMARY KEY (id),
+	CONSTRAINT players_game_id_fkey FOREIGN KEY (game_id) REFERENCES public.games(id) ON DELETE CASCADE,
+	CONSTRAINT players_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE,
+	CONSTRAINT players_word_id_fkey FOREIGN KEY (word_id) REFERENCES public.words(id)
+);
+CREATE INDEX idx_players_game ON public.players USING btree (game_id);
+
+
+CREATE TABLE public.room_users (
+	room_id int4 NOT NULL,
+	user_id int4 NOT NULL,
+	joined_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT room_players_pkey PRIMARY KEY (room_id, user_id),
+	CONSTRAINT fk_room_users FOREIGN KEY (user_id) REFERENCES public.users(id),
+	CONSTRAINT room_players_player_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE,
+	CONSTRAINT room_players_room_id_fkey FOREIGN KEY (room_id) REFERENCES public.rooms(id) ON DELETE CASCADE
 );
 
-CREATE TABLE games (
-  id SERIAL PRIMARY KEY,
-  room_id INT NOT NULL,
-  winner player_role,
-  status game_status DEFAULT 'WAITING' NOT NULL,
-  round_number INT DEFAULT 1 NOT NULL,
-  started_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  finished_at TIMESTAMP,
 
-  FOREIGN KEY (room_id) REFERENCES rooms(id) ON DELETE CASCADE
+CREATE TABLE public.rooms (
+	id serial4 NOT NULL,
+	"name" varchar(100) NOT NULL,
+	host_id int4 NOT NULL,
+	status public."room_status" DEFAULT 'WAITING'::room_status NOT NULL,
+	max_users int4 DEFAULT 3 NOT NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	closed_at timestamp NULL,
+	CONSTRAINT rooms_pkey PRIMARY KEY (id),
+	CONSTRAINT rooms_host_id_fkey FOREIGN KEY (host_id) REFERENCES public.users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE words (
-  id SERIAL PRIMARY KEY,
-  word VARCHAR(50) UNIQUE NOT NULL,
-  impostor_clue TEXT NOT NULL
+
+CREATE TABLE public.users (
+	id serial4 NOT NULL,
+	"name" varchar(100) NOT NULL,
+	email varchar(150) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"role" varchar(20) DEFAULT 'PLAYER'::character varying NULL,
+	CONSTRAINT users_email_key UNIQUE (email),
+	CONSTRAINT users_name_key UNIQUE (name),
+	CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE game_words (
-  game_id INT PRIMARY KEY,
-  word_id INT NOT NULL,
 
-  FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-  FOREIGN KEY (word_id) REFERENCES words(id)
+CREATE TABLE public.votes (
+	round_number int4 NOT NULL,
+	game_id int4 NOT NULL,
+	voter_id int4 NOT NULL,
+	target_player_id int4 NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT votes_pkey PRIMARY KEY (round_number, game_id, voter_id),
+	CONSTRAINT votes_game_id_fkey FOREIGN KEY (game_id) REFERENCES public.games(id) ON DELETE CASCADE,
+	CONSTRAINT votes_target_player_id_fkey FOREIGN KEY (target_player_id) REFERENCES public.players(id) ON DELETE SET NULL,
+	CONSTRAINT votes_voter_id_fkey FOREIGN KEY (voter_id) REFERENCES public.players(id) ON DELETE CASCADE
 );
+CREATE INDEX idx_votes_game ON public.votes USING btree (game_id);
 
-CREATE TABLE players (
-  player_id SERIAL PRIMARY KEY,
-  game_id INT NOT NULL,
-  user_id INT NOT NULL,
-  word_id INT NOT NULL,
-  role player_role DEFAULT 'INNOCENT' NOT NULL,
-  is_alive BOOLEAN DEFAULT TRUE NOT NULL,
-
-  UNIQUE (game_id, user_id),
-
-  FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  FOREIGN KEY (word_id) REFERENCES words(id)
+CREATE TABLE public.words (
+	id serial4 NOT NULL,
+	word varchar(50) NOT NULL,
+	impostor_clue text NOT NULL,
+	CONSTRAINT unique_word UNIQUE (word),
+	CONSTRAINT words_pkey PRIMARY KEY (id),
+	CONSTRAINT words_word_key UNIQUE (word)
 );
-
-CREATE TABLE messages (
-  id SERIAL PRIMARY KEY,
-  game_id INT NOT NULL,
-  player_id INT NOT NULL,
-  content TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-
-  FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-  FOREIGN KEY (player_id) REFERENCES players(player_id) ON DELETE CASCADE
-);
-
-CREATE TABLE clues (
-  id SERIAL PRIMARY KEY,
-  game_id INT NOT NULL,
-  player_id INT NOT NULL,
-  round_number INT NOT NULL,
-  clue TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-
-  FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-  FOREIGN KEY (player_id) REFERENCES players(player_id) ON DELETE CASCADE
-);
-
-CREATE TABLE votes (
-  round_number INT,
-  game_id INT,
-  voter_id INT,
-  target_player_id INT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-
-  PRIMARY KEY (round_number, game_id, voter_id),
-
-  FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-  FOREIGN KEY (voter_id) REFERENCES players(player_id) ON DELETE CASCADE,
-  FOREIGN KEY (target_player_id) REFERENCES players(player_id) ON DELETE SET NULL
-);
-
-CREATE INDEX idx_players_game ON players(game_id);
-CREATE INDEX idx_votes_game ON votes(game_id);
-CREATE INDEX idx_clues_game ON clues(game_id);
-CREATE INDEX idx_messages_game ON messages(game_id);
